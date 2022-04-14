5b464630996e0129e617bb7172ddf78a10fd75d1
JosephShin <joespam9999@gmail.com>
2022-04-14 10:37:10 -0400

    commit tree 302b972e6511607d2ffb7ee94fb7f4f3dc03cc9a

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//add hidden class to error modal
+const errorModal = document.getElementById('modal')
+errorModal.className = 'hidden'
 
-
+//click event for empty heart
+const likeGlyph = document.getElementsByClassName('like-glyph')
+Array.from(likeGlyph).forEach(elem => {
+    elem.addEventListener('click', (e) => {
+      mimicServerCall()
+        .then(data => {
+          //debugger
+          if(elem.textContent === EMPTY_HEART){
+            elem.textContent = FULL_HEART
+            elem.className = 'activated-heart'
+          }else if (elem.textContent === FULL_HEART){
+            elem.textContent = EMPTY_HEART
+            elem.classList.remove('activated-heart')
+          }
+        })
+        .catch(error => {
+          //display error modal by removing .hidden class tag
+          errorModal.classList.remove('hidden')
+          errorModal.querySelector('#modal-message').textContent = error
+          setTimeout(function(){errorModal.className = 'hidden'},3000)
+        })
+      //debugger
+    })
+  
+}) 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
